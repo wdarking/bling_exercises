100a54ccc03cf70146cdc35f1ffe786d9988b2e1
Gilmar Pereira <wdarking@gmail.com>
2020-12-24 17:28:14 -0300

    Remove extra lines

--- Pseudocode.php
+++ Pseudocode.php
@@ -42,15 +42,3 @@ class ComponentIterator {
         return $list;
     }
 }
-
-atores->filmes: n,n
-filmes->atores n,n
-
-SELECT * FROM `atores` JOIN ator_filme ON atores.id = ator_filme.ator_id JOIN filmes ON ator_filme.filme_id = filmes.id WHERE filmes.titulo = 'XYZ'
-
-SELECT * FROM `filmes` JOIN ator_filme ON filmes.id = ator_filme.filme_id JOIN atores ON ator_filme.ator_id = atores.id WHERE atores.nome = 'FULANO' AND ator_filme.funcao = 'participante'
-
-SELECT filmes.titulo, filmes.ano, COUNT(filmes.id) as total_participantes FROM filmes JOIN ator_filme ON filmes.id = ator_filme.filme_id WHERE filmes.ano = '2015' GROUP BY filmes.id
-ORDER BY `filmes`.`titulo` ASC, `total_participantes` DESC
-
-SELECT * FROM `atores` JOIN ator_filme ON ator_filme.ator_id = atores.id WHERE ator_filme.filme_id IN (SELECT filmes.id FROM `filmes` WHERE filmes.id IN (SELECT ator_filme.filme_id FROM ator_filme JOIN atores ON atores.id = ator_filme.ator_id WHERE atores.nome = 'SPIELBERG' and ator_filme.funcao = 'diretor')) AND ator_filme.funcao = 'participante'
